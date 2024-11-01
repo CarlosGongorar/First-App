27817726bf8a0311cd45772f9fd4e9962ab60cfb
CarlosGongorar <carlosgongorar50@gmail.com>
2024-10-31 22:28:16 -0500

    Form Validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Formik, useField } from 'formik';
 import { StyleSheet, View, Button} from 'react-native';
 import StyledTextInput from '../components/StyledTextInput';
+import StyledText from "../components/StyledText"
 
 const initialValues = {
     email: "",
@@ -9,6 +10,10 @@ const initialValues = {
 }
 
 const styles = StyleSheet.create({
+    error: {
+        color: "red",
+        marginBottom: 20,
+    },
     form: {
         margin: 12
     }
@@ -17,11 +22,31 @@ const styles = StyleSheet.create({
 const FormikInputValue = ({ name, ...props}) =>{
     const [field, meta, helpers] = useField(name)
     return (
-        <StyledTextInput value = {field.value} onChangeText={value => helpers.setValue(value)} {...props}/>
+        <>
+        <StyledTextInput error={meta.error} value = {field.value} onChangeText={value => helpers.setValue(value)} {...props}/>
+            {meta.error && <StyledText style={styles.error}>
+                {meta.error}
+            </StyledText>}
+        </>
     )
 }
+
+const validate = values => {
+    const errors ={}
+
+    if (!values.email) {
+        errors.email = "E-mail is required"
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9]+\.[A-Z]{2,4}$/i.
+    test(values.email)) {
+        errors.email = "Invalid email address"
+    }
+    console.log(errors)
+    
+    return errors
+}
+
 export default function LogInPage() {
-    return <Formik initialValues={initialValues} onSubmit={values => console.log(values)}>
+    return <Formik validate={validate} initialValues={initialValues} onSubmit={values => console.log(values)}>
         {({handleSubmit}) => {
             return <View style={styles.form}>
                 <FormikInputValue name="email" placeholder='E-mail' />
